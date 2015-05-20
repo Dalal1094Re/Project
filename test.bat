26e01039d8b3ebb69af1bc98c1abc04b31e60be0
Dalal1094Re <renuka.dalal@sitpune.edu.in>
2015-05-20 11:15:26 +0530

    Addition of files

---
+++ test.bat
@@ -0,0 +1,8 @@
+mkdir c:\Project
+pause
+cd C:\Users\renuka_dalal\Project
+pause
+git init
+echo.>"Read.txt"
+pause
+
---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd 'C:\Users\renuka_dalal\Project'
+sleep 5
+git init
+sleep 5
+git status
+sleep 10
+git add -A
+sleep 5
+git commit -a -m "Addition of files"
+sleep 10
+git push origin master
+sleep 15
