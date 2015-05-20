701d04fc86fc6dec92f5bf32549d2ac9b10cc879
Dalal1094Re <renuka.dalal@sitpune.edu.in>
2015-05-20 11:37:24 +0530

    Addition of files

--- test.sh
+++ test.sh
@@ -10,6 +10,6 @@ git add -A
 sleep 5
 git commit -a -m "Addition of files"
 sleep 10
-git remote set-url origin ssh://git@github.com/Dalal1094Re/Project.git
+git config remote.origin.url https://Dalal1094Re:manvikaran31094@github.com/Dalal1094Re/Project.git
 git push origin master
 sleep 15
