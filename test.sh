df6450fcfa3a9b8b928994a2795856694f1502bf
Dalal1094Re <renuka.dalal@sitpune.edu.in>
2015-05-20 16:13:26 +0530

    Addition of files

--- test.sh
+++ test.sh
@@ -3,7 +3,6 @@ cd 'C:\Users\renuka_dalal\Project'
 sleep 5
 git init
 sleep 5
-touch Red.txt
 git status
 sleep 5
 git add -p
@@ -11,6 +10,7 @@ sleep 5
 git add -A
 sleep 5
 git commit -a -m "Updation"
+echo "Commit successfull"
 sleep 10
 git config remote.origin.url https://Dalal1094Re:manvikaran31094@github.com/Dalal1094Re/Project.git
 git push origin master
