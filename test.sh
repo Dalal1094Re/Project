3b59d31816ca20c27c2c745bd8db8b0318dc9c99
Dalal1094Re <renuka.dalal@sitpune.edu.in>
2015-05-20 11:26:37 +0530

    Addition of files

--- test.sh
+++ test.sh
@@ -3,11 +3,13 @@ cd 'C:\Users\renuka_dalal\Project'
 sleep 5
 git init
 sleep 5
+touch Red.txt
 git status
 sleep 10
 git add -A
 sleep 5
 git commit -a -m "Addition of files"
 sleep 10
+git remote add origin ssh://git@github.com/Dalal1094Re/Project.git
 git push origin master
 sleep 15
